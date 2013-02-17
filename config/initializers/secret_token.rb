35aa902bf50504be02ca1f2e19b79743b35cef21
Dru <drurly@gmail.com>
2013-02-17 10:10:57 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pulse::Application.config.secret_token = '90c17af3fbcb1d1ba44e9f23feadb92a15127afc87ad512b94e58f3697c4badcf1b15f08d7c89a18ee246809bc91552728d4c29a8d793d8ba8ded14813a8b7b6'
