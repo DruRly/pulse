66d24bcb18379ec32b523ea964b101e9f9ee1418
Dru Riley <dev+Dru@blinqmedia.com>
2013-02-18 18:10:03 -0500

    WIP: Integrate Highcharts

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'json'
 
 gem 'googlecharts'
 
+gem 'lazy_high_charts'
+
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def index
-    @counts = Petition.all.map(&:last_365_days_signature_counts)
+    #@counts = Petition.all.map(&:last_365_days_signature_counts)
+    @h = LazyHighCharts::HighChart.new('graph') do |f|
+      f.options[:chart][:defaultSeriesType] = "area"
+      Petition.all.map do |p|
+        f.series(data: p.last_365_days_signature_counts, showInLegend: false)
+      end
+    end
   end
 end
