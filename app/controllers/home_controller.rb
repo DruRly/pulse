e9d091e2762d79301d2930abe2f4afa5ec44ebb7
Dru <drurly@gmail.com>
2013-02-22 12:09:08 -0500

    WIP: Select petitions based on issues

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,16 @@
 class HomeController < ApplicationController
   def index
+    @issues = Issue.all.map(&:name).unshift("All Issues")
+    if (params[:issue] && Petition.all.select {|p| p.issues.map(&:name).include?(params[:issue])})
+      @petitions = Petition.all.select { |p| p.issues.map(&:name).include?(params[:issue]) }
+      #selected
+    else
+      #non specific
+    end
+
     @h = LazyHighCharts::HighChart.new('graph') do |f|
       f.options[:chart][:defaultSeriesType] = "area"
+      #this is going to use the petitions class variable
       Petition.first(3).map do |p|
         f.series(data: p.days_growth_rates(90), showInLegend: false, name: p.title)
       end
@@ -9,6 +18,5 @@ class HomeController < ApplicationController
 
     @rankings = Petition.top_by_average(5)
     @near_threshold = Petition.near_threshold(5)
-    @issues = Issue.all.map(&:name).unshift("All Issues")
   end
 end
